7dc7e8e745a4938f8aacaa620562ada26df05ae8
BenAc <b.feldberg.collins@accenture.com>
2022-09-07 14:00:32 +0100

    ticket-102 add employee payroll and fix bug

--- types.ts
+++ types.ts
@@ -6,7 +6,7 @@ export type Day =
     | 'Tuesday'
     | 'Wednesday'
     | 'Thursday'
-    | 'Fripay'
+    | 'Friday'
     | 'Saturday'
 
 export type Rota = {
@@ -14,4 +14,37 @@ export type Rota = {
         day: Day
         employee: string
     }[]
+}
+
+export type TeamName =
+    | 'Leadership'
+    | 'Customer Service'
+    | 'Data Analysis and Research'
+
+export type Team = {
+    name: TeamName
+    members: Employee[]
+}
+
+export type Roles =
+    | 'Manager'
+    | 'Team Lead'
+    | 'Customer Service Agent'
+    | 'Data Analyst'
+
+export type Splines = 1 | 2 | 3
+
+export type Employee = {
+    id: string
+    startDate: string
+    name: string
+    age: number
+    role: Roles
+    spline: Splines
+}
+
+export type Payroll = {
+    id: string
+    salary: number
+    spline: Splines
 }
