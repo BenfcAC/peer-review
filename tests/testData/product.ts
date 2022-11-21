98b6df22ebd41e01b6ae1d92ccdaf4eac021174f
BenAc <b.feldberg.collins@accenture.com>
2022-11-21 14:07:55 +0000

    move test data

--- tests/testData/product.ts
+++ tests/testData/product.ts
@@ -1,6 +1,14 @@
-import { Product } from '../../src/types';
+import { Product, SaleProduct } from '../../src/types';
 
 export const aProduct = (): Product => ({
     price: 10,
     sku: '12-3',
+    onSale: false,
+});
+
+export const aSaleProduct = (): SaleProduct => ({
+    price: 10,
+    sku: '12-3',
+    onSale: true,
+    discount: 20,
 });
